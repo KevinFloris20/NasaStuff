d65a185df4f9e41cc9e6a11ee0780e61fc1b945d
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-08-06 23:30:31 -0400

    first picture api

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+const quearyUrl = "https://api.nasa.gov/planetary/apod?api_key=4INjsmvRu2ZfwkrdUPUbqk0cpCXP4fpvVQlN4qBw";
+    $.ajax({
+        url: quearyUrl,
+        method: "GET"
+    }).then(function(responce){
+        console.log(responce);
+        console.log(responce.hdurl);
+        document.getElementById("picture").setAttribute("src", responce.hdurl);
+    })
+
