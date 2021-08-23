95811318d671ad550a97e5936996d7532c283d27
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-08-22 20:44:46 -0400

    Made collapsibles more mobile friendly and began to make background reactive

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,19 @@
 //Get Nasa API and make it background
 const quearyUrl = "https://api.nasa.gov/planetary/apod?api_key=4INjsmvRu2ZfwkrdUPUbqk0cpCXP4fpvVQlN4qBw";
+function NasaBackground(){
+    console.log('yo');
     $.ajax({
         url: quearyUrl,
         method: "GET"
     }).then(function(responce){
         console.log(responce);
         console.log(responce.hdurl);
-        document.body.style.size = "auto";
+        document.body.style.size = "fill";
         document.body.style.backgroundImage = "url("+responce.hdurl+")";
     })
+}
+NasaBackground();
+
 console.log(window.devicePixelRatio)//for personal ref
 
 //animate and make the collapsible buttons functional
@@ -20,8 +25,10 @@ for (var i = 0; i < coll.length; i++) {
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
+      NasaBackground();
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
+      NasaBackground();
     } 
   });
 }
